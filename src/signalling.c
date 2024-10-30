b72215076749abc25575e027ebd992bafc596157
Tharnath <itstharnath@gmail.com>
2024-10-30 13:20:43 -0600

    Signal.c

--- src/signalling.c
+++ src/signalling.c
@@ -10,6 +10,13 @@
 void signal_handle_calculation(SemaphoreHandle_t request,
                                SemaphoreHandle_t response,
                                struct signal_data *data){
+                                printf("Waiting for Semaphore\n");
+                                xSemaphoreTake(request, portMAX_DELAY);
+                                printf("Handling Semaphores\n");
+                                data->output = data->input+5; //in Signals.h data has ip&op
+                                vTaskDelay(10);
+                                printf("Sending Result\n");
+                                xSemaphoreGive(response);
 
                                 return;
                                }
@@ -17,8 +24,10 @@ void signal_handle_calculation(SemaphoreHandle_t request,
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data){
-                                        xSemaphoreTake(request, portMAX_DELAY);
-                                        data->output = data->input + 5;
+                                       // xSemaphoreTake(request, portMAX_DELAY);
+                                        printf("Giving Semaphore\n");
                                         xSemaphoreGive(request);
-                                        return ;
+                                        BaseType_t res = xSemaphoreTake(response,portMAX_DELAY);
+                                        printf("- Result ready\n");
+                                        return res;
                                     }
